fbe2d4789b62d52b027bd7ad96d4811d02178ed8
Flynn <wphestiraid@gmail.com>
2024-01-25 10:51:37 +0900

    :fire: 미들웨서 설정 부분 삭제

--- app/main.py
+++ app/main.py
@@ -1,18 +1,16 @@
 from fastapi import FastAPI
 
-from app.middlewares import init_middlewares
 from app.routers import init_routers
 
 
 def create_app() -> FastAPI:
     app = FastAPI(
-        title='FastAPI Base',
-        description='FastAPI Base Project',
-        version='0.1.0',
+        title="FastAPI Base",
+        description="FastAPI Base Project",
+        version="0.1.0",
     )
 
     init_routers(app)
-    init_middlewares(app)
 
     return app
 
--- app/middlewares/__init__.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-
-
-def init_middlewares(app: FastAPI) -> None:
-    middlewares = []
-
-    for middleware in middlewares:
-        app.add_middleware(middleware)
